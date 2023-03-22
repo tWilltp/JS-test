2730031209fa904bcc8521e7c0c41c3451433183
tWilltp <williamjenkinsfeb@gmail.com>
2023-03-22 16:36:00 +0000

    http.server

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,19 @@
+const buttonClick = require("../button");
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFilesync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("DOM tests", () => {
+    test("Expects content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+});
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -4,7 +4,10 @@ describe("calculator", () => {
     describe("addition function", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
-        })    
+        })
+        test("should return 73 for 41 + 32", () => {
+            expect(addition(41, 32)).toBe(73);
+        })   
     });
     describe("subtaction function", () => {
 
